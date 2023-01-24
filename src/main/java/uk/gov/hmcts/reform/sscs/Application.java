81a0b4fc8e8c3612ee6d881d714def3a059f9f3b
TeaWithLucas <18538046+TeaWithLucas@users.noreply.github.com>
2023-01-24 13:06:29 +0000

    Fixes

--- src/main/java/uk/gov/hmcts/reform/sscs/Application.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/Application.java
@@ -1,13 +1,38 @@
 package uk.gov.hmcts.reform.sscs;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import uk.gov.hmcts.reform.sscs.ccd.config.CcdRequestDetails;
 
 @SpringBootApplication
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
+@ComponentScan(basePackages = {"uk.gov.hmcts.reform"})
+@EnableFeignClients(basePackages =
+    {
+        "uk.gov.hmcts.reform.sscs.service",
+        "uk.gov.hmcts.reform.authorisation",
+        "uk.gov.hmcts.reform.sscs.idam",
+        "uk.gov.hmcts.reform.sscs.service",
+        "uk.gov.hmcts.reform.idam"
+    })
 public class Application {
 
     public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public CcdRequestDetails getRequestDetails(
+        @Value("${core_case_data.jurisdictionId}") String coreCaseDataJurisdictionId,
+        @Value("${core_case_data.caseTypeId}") String coreCaseDataCaseTypeId) {
+        return CcdRequestDetails.builder()
+            .caseTypeId(coreCaseDataCaseTypeId)
+            .jurisdictionId(coreCaseDataJurisdictionId)
+            .build();
     }
 }
