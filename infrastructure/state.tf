23d16295ff137e382359e159eb94d39ff0183759
TeaWithLucas <18538046+TeaWithLucas@users.noreply.github.com>
2023-01-25 13:04:48 +0000

    infrastructure

--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -1,3 +1,16 @@
 terraform {
-  backend "azurerm" {}
+  backend "azurerm" {
+    features {
+      key_vault {
+        purge_soft_delete_on_destroy = false
+      }
+    }
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.6.0"
+    }
+  }
 }
