0ba106ca0b239ca6182e4edfc143512c6c63cdbc
TeaWithLucas <18538046+TeaWithLucas@users.noreply.github.com>
2023-01-26 14:36:24 +0000

    Terraform changes

--- infrastructure/service-bus.tf
+++ infrastructure/service-bus.tf
@@ -1,55 +1,17 @@
 #HMC to Hearings API
 module "servicebus-subscription" {
   source              = "git@github.com:hmcts/terraform-module-servicebus-subscription?ref=master"
-  name                = "hmc-to-sscs-subscription-${var.env}"
-  namespace_name      = "hmc-servicebus-${var.env}"
-  topic_name          = "hmc-to-cft-${var.env}"
-  resource_group_name = "hmc-shared-${var.env}"
+  name                = "mock-hmc-to-sscs-subscription-${var.env}"
+  namespace_name      = "sscs-servicebus-${var.env}"
+  topic_name          = "mock-hmc-to-cft-${var.env}"
+  resource_group_name = "sscs-${var.env}"
 }
 
-resource "azurerm_servicebus_subscription_rule" "topic_filter_rule_sscs" {
-  name            = "hmc-servicebus-${var.env}-subscription-rule-bba3"
-  subscription_id = module.servicebus-subscription.id
-  filter_type     = "CorrelationFilter"
-
-  correlation_filter {
-    properties = {
-      hmctsServiceId = "BBA3"
-    }
-  }
-}
-
-data "azurerm_key_vault" "hmc-key-vault" {
-  name                = "hmc-${var.env}"
-  resource_group_name = "hmc-shared-${var.env}"
-}
-
-data "azurerm_key_vault_secret" "hmc-servicebus-connection-string" {
-  key_vault_id = data.azurerm_key_vault.hmc-key-vault.id
-  name         = "hmc-servicebus-connection-string"
-}
-
-resource "azurerm_key_vault_secret" "hmc-servicebus-connection-string" {
-  name         = "hmc-servicebus-connection-string"
-  value        = data.azurerm_key_vault_secret.hmc-servicebus-connection-string.value
+resource "azurerm_key_vault_secret" "mock-hmc-servicebus-shared-access-key-tf" {
+  name         = "mock-hmc-servicebus-shared-access-key-tf"
+  value        = data.azurerm_key_vault_secret.mock-hmc-servicebus-shared-access-key.value
   key_vault_id = data.azurerm_key_vault.sscs_key_vault.id
 
-  content_type = "secret"
-  tags = merge(var.common_tags, {
-    "source" : "Vault ${data.azurerm_key_vault.sscs_key_vault.name}"
-  })
-}
-
-data "azurerm_key_vault_secret" "hmc-servicebus-shared-access-key" {
-  key_vault_id = data.azurerm_key_vault.hmc-key-vault.id
-  name         = "hmc-servicebus-shared-access-key"
-}
-
-resource "azurerm_key_vault_secret" "sscs-hmc-servicebus-shared-access-key-tf" {
-  name         = "hmc-servicebus-shared-access-key-tf"
-  value        = data.azurerm_key_vault_secret.hmc-servicebus-shared-access-key.value
-  key_vault_id = data.azurerm_key_vault.sscs_key_vault.id
-  
   content_type = "secret"
   tags = merge(var.common_tags, {
     "source" : "Vault ${data.azurerm_key_vault.sscs_key_vault.name}"
