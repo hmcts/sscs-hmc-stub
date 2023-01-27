7f2b6655b05e1f748a6466f3f74e97436bcc8a41
Lucas <18538046+TeaWithLucas@users.noreply.github.com>
2023-01-27 16:16:19 +0000

    Fix Jacoco (#25)
    * Fix Jacoco
    
    * Update dependency hashicorp/terraform to v1
    
    * reenable pmd
    
    * Update service-bus.tf
    
    * Update service-bus.tf

--- infrastructure/service-bus.tf
+++ infrastructure/service-bus.tf
@@ -1,19 +1,7 @@
 #HMC to Hearings API
-module "servicebus-subscription" {
-  source              = "git@github.com:hmcts/terraform-module-servicebus-subscription?ref=master"
-  name                = "mock-hmc-to-sscs-subscription-${var.env}"
-  namespace_name      = "sscs-servicebus-${var.env}"
-  topic_name          = "mock-hmc-to-cft-${var.env}"
-  resource_group_name = "sscs-${var.env}"
-}
-
-resource "azurerm_key_vault_secret" "mock-hmc-servicebus-shared-access-key-tf" {
-  name         = "mock-hmc-servicebus-shared-access-key-tf"
-  value        = data.azurerm_key_vault_secret.mock-hmc-servicebus-shared-access-key.value
-  key_vault_id = data.azurerm_key_vault.sscs_key_vault.id
-
-  content_type = "secret"
-  tags = merge(var.common_tags, {
-    "source" : "Vault ${data.azurerm_key_vault.sscs_key_vault.name}"
-  })
+module "servicebus-topic" {
+  source                = "git@github.com:hmcts/terraform-module-servicebus-topic?ref=master"
+  name                  = "mock-hmc-to-cft-${var.env}"
+  namespace_name        = "sscs-servicebus-${var.env}"
+  resource_group_name   = "sscs-${var.env}"
 }
