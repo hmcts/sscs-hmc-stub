3ecac9cb1a4b922f347305f82f28351e86e55cf9
TeaWithLucas <18538046+TeaWithLucas@users.noreply.github.com>
2023-01-27 17:01:09 +0000

    using terraform values

--- infrastructure/service-bus.tf
+++ infrastructure/service-bus.tf
@@ -5,3 +5,47 @@ module "servicebus-topic" {
   namespace_name        = "sscs-servicebus-${var.env}"
   resource_group_name   = "sscs-${var.env}"
 }
+
+resource "azurerm_key_vault_secret" "sscs-servicebus-topic-namespace-name" {
+  name = "sscs-servicebus-topic-namespace-name"
+  value = "sscs-servicebus-${var.env}"
+  key_vault_id = data.azurerm_key_vault.sscs_key_vault.id
+}
+
+resource "azurerm_key_vault_secret" "stub-hmc-servicebus-topic-name" {
+  name = "stub-hmc-servicebus-topic-name"
+  value = module.servicebus-topic.name
+  key_vault_id = data.azurerm_key_vault.sscs_key_vault.id
+}
+
+resource "azurerm_key_vault_secret" "stub-hmc-servicebus-policy-name" {
+  name = "stub-hmc-servicebus-policy-name"
+  value = module.servicebus-topic.azurerm_servicebus_topic_authorization_rule.send_listen_auth_rule.name
+  key_vault_id = data.azurerm_key_vault.sscs_key_vault.id
+}
+
+resource "azurerm_key_vault_secret" "stub-hmc-servicebus-shared-access-key-tf" {
+  name = "mock-hmc-servicebus-shared-access-key-tf"
+  value = module.servicebus-topic.azurerm_servicebus_topic_authorization_rule.send_listen_auth_rule.primary_key
+  key_vault_id = data.azurerm_key_vault.sscs_key_vault.id
+}
+
+resource "azurerm_key_vault_secret" "stub-hmc-servicebus-policy-name" {
+  name = "stub-hmc-servicebus-policy-name"
+  value = module.servicebus-topic.azurerm_servicebus_topic_authorization_rule.send_listen_auth_rule.name
+  key_vault_id = data.azurerm_key_vault.sscs_key_vault.id
+}
+
+module "servicebus-subscription" {
+  source              = "git@github.com:hmcts/terraform-module-servicebus-subscription?ref=master"
+  name                = "hmc-to-sscs-subscription-${var.env}"
+  namespace_name      = "hmc-servicebus-${var.env}"
+  topic_name          = "stub-hmc-to-cft-${var.env}"
+  resource_group_name = "hmc-shared-${var.env}"
+}
+
+resource "azurerm_key_vault_secret" "stub-hmc-servicebus-subscription-name" {
+  name = "stub-hmc-servicebus-subscription-name"
+  value = module.servicebus-topic.servicebus-subscription.name
+  key_vault_id = data.azurerm_key_vault.sscs_key_vault.id
+}
