c28697176ca771734b10dd23da88920ff3f2638a
TeaWithLucas <18538046+TeaWithLucas@users.noreply.github.com>
2023-01-27 16:30:45 +0000

    Update service-bus.tf

--- infrastructure/service-bus.tf
+++ infrastructure/service-bus.tf
@@ -1,7 +1,7 @@
 #HMC to Hearings API
 module "servicebus-topic" {
   source                = "git@github.com:hmcts/terraform-module-servicebus-topic?ref=master"
-  name                  = "${SERVICE_NAME}-stub-hmc-to-cft-${var.env}"
+  name                  = "stub-hmc-to-cft-${var.env}"
   namespace_name        = "sscs-servicebus-${var.env}"
   resource_group_name   = "sscs-${var.env}"
 }
