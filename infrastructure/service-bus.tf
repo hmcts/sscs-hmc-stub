328c007d690ab5014a809b082de364f7bfa5a2c4
TeaWithLucas <18538046+TeaWithLucas@users.noreply.github.com>
2023-01-27 17:52:42 +0000

    Update service-bus.tf

--- infrastructure/service-bus.tf
+++ infrastructure/service-bus.tf
@@ -10,24 +10,44 @@ resource "azurerm_key_vault_secret" "sscs-servicebus-topic-namespace-name" {
   name = "sscs-servicebus-topic-namespace-name"
   value = "sscs-servicebus-${var.env}"
   key_vault_id = data.azurerm_key_vault.sscs_key_vault.id
+
+    content_type = "secret"
+    tags = merge(var.common_tags, {
+      "source" : "Vault ${data.azurerm_key_vault.sscs_key_vault.name}"
+    })
 }
 
 resource "azurerm_key_vault_secret" "stub-servicebus-topic-name" {
   name = "stub-servicebus-topic-name"
   value = module.servicebus-topic.name
   key_vault_id = data.azurerm_key_vault.sscs_key_vault.id
+
+    content_type = "secret"
+    tags = merge(var.common_tags, {
+      "source" : "Vault ${data.azurerm_key_vault.sscs_key_vault.name}"
+    })
 }
 
 resource "azurerm_key_vault_secret" "stub-servicebus-policy-name" {
   name = "stub-servicebus-policy-name"
   value = "SendAndListenSharedAccessKey"
   key_vault_id = data.azurerm_key_vault.sscs_key_vault.id
+
+    content_type = "secret"
+    tags = merge(var.common_tags, {
+      "source" : "Vault ${data.azurerm_key_vault.sscs_key_vault.name}"
+    })
 }
 
 resource "azurerm_key_vault_secret" "stub-servicebus-shared-access-key-tf" {
   name = "stub-servicebus-shared-access-key-tf"
   value = module.servicebus-topic.primary_send_and_listen_shared_access_key
   key_vault_id = data.azurerm_key_vault.sscs_key_vault.id
+
+    content_type = "secret"
+    tags = merge(var.common_tags, {
+      "source" : "Vault ${data.azurerm_key_vault.sscs_key_vault.name}"
+    })
 }
 
 module "servicebus-subscription" {
@@ -44,4 +64,9 @@ resource "azurerm_key_vault_secret" "stub-servicebus-subscription-name" {
   name = "stub-servicebus-subscription-name"
   value = "stub-to-sscs-subscription-${var.env}"
   key_vault_id = data.azurerm_key_vault.sscs_key_vault.id
+
+    content_type = "secret"
+    tags = merge(var.common_tags, {
+      "source" : "Vault ${data.azurerm_key_vault.sscs_key_vault.name}"
+    })
 }
