2aac46d28daec76e9bd5d2e3925b8bb7343c2fc2
TeaWithLucas <18538046+TeaWithLucas@users.noreply.github.com>
2023-01-27 15:53:55 +0000

    Update service-bus.tf

--- infrastructure/service-bus.tf
+++ infrastructure/service-bus.tf
@@ -6,14 +6,3 @@ module "servicebus-subscription" {
   topic_name          = "mock-hmc-to-cft-${var.env}"
   resource_group_name = "sscs-${var.env}"
 }
-
-resource "azurerm_key_vault_secret" "mock-hmc-servicebus-shared-access-key-tf" {
-  name         = "mock-hmc-servicebus-shared-access-key-tf"
-  value        = data.azurerm_key_vault_secret.mock-hmc-servicebus-shared-access-key.value
-  key_vault_id = data.azurerm_key_vault.sscs_key_vault.id
-
-  content_type = "secret"
-  tags = merge(var.common_tags, {
-    "source" : "Vault ${data.azurerm_key_vault.sscs_key_vault.name}"
-  })
-}
